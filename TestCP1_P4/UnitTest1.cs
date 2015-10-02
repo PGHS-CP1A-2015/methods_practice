6133280124ffc89b2f56d960a8fc7b5597ad0121
kajigga <kajigga@gmail.com>
2015-10-02 11:06:13 -0600

    I fixed the incorrect values in the UnitTest1.cs file. Circumference and area of circles tests should work properly.

--- TestCP1_P4/UnitTest1.cs
+++ TestCP1_P4/UnitTest1.cs
@@ -29,7 +29,7 @@ namespace TestCP1_P4
             Assert.AreEqual(-12, P4_2.MyMathFunctions.fahrenheitToCelsius(10));
             Assert.AreEqual(-40, P4_2.MyMathFunctions.fahrenheitToCelsius(-40));
         }
-        /*
+        
         [TestMethod]
         public void Test_celsius_to_fahrenheit()
         {
@@ -40,20 +40,20 @@ namespace TestCP1_P4
         [TestMethod]
         public void Test_calculate_circumference_of_circle()
         {
-            Assert.AreEqual(12.56, P4_2.MyMathFunctions.circumferenceOfCircle(2));
-            Assert.AreEqual(75.36, P4_2.MyMathFunctions.circumferenceOfCircle(12));
-            Assert.AreEqual(34.54, P4_2.MyMathFunctions.circumferenceOfCircle(5.5));
-            Assert.AreEqual(43.96, P4_2.MyMathFunctions.circumferenceOfCircle(7));
+            Assert.AreEqual(12.57, P4_2.MyMathFunctions.circumferenceOfCircle(2));
+            Assert.AreEqual(75.4, P4_2.MyMathFunctions.circumferenceOfCircle(12));
+            Assert.AreEqual(34.56, P4_2.MyMathFunctions.circumferenceOfCircle(5.5));
+            Assert.AreEqual(43.98, P4_2.MyMathFunctions.circumferenceOfCircle(7));
         }
 
         [TestMethod]
         public void Test_calculate_area_of_a_circle()
         {
 
-            Assert.AreEqual(12.56, P4_2.MyMathFunctions.areaOfCircle(2));
-            Assert.AreEqual(314, P4_2.MyMathFunctions.areaOfCircle(10));
-            Assert.AreEqual(94.985, P4_2.MyMathFunctions.areaOfCircle(5.5));
-            Assert.AreEqual(153.86, P4_2.MyMathFunctions.areaOfCircle(7));
+            Assert.AreEqual(12.57, P4_2.MyMathFunctions.areaOfCircle(2));
+            Assert.AreEqual(314.16, P4_2.MyMathFunctions.areaOfCircle(10));
+            Assert.AreEqual(95.03, P4_2.MyMathFunctions.areaOfCircle(5.5));
+            Assert.AreEqual(153.94, P4_2.MyMathFunctions.areaOfCircle(7));
         }
         [TestMethod]
         public void Test_returns_theSameStringDoubled()
@@ -81,7 +81,7 @@ namespace TestCP1_P4
             int remainder = number1 % number2;
             Assert.AreEqual(remainder, P4_2.MyMathFunctions.getRemainder(number1, number2));
         }
-        */
+        /**/
         /* Don't worry about anything below this point */
         private Random _randomGenerator;
         private int random_number(int v1, int v2)
